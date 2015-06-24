b73f2276bde4e498646637204d13d4e9261b0f90
Stuart Grieve <s.grieve@ed.ac.uk>
2015-06-24 09:57:43 +0100

    New hold theta method added

--- ShieldingSensitivity.py
+++ ShieldingSensitivity.py
@@ -250,4 +250,61 @@ def resid_comparison_hold_phi():
         plt.savefig('Phi_Zenith_resid_'+Labels[0].split(',')[1].strip(')')+'.png')
         plt.clf()
         
-resid_comparison_hold_phi()
+#resid_comparison_hold_phi()
+
+
+def resid_comparison_hold_theta():
+
+    path = 'C:\\Users\\Stuart\\Desktop\\FR\\cosmo\\'
+    
+    theta_list = [1, 2, 3, 5, 8, 10, 15, 20, 30, 45, 60, 90, 180, 360]
+    #file_prefix = 'z33_26586_'
+    file_prefix = 'z33_17218_'
+    
+    for t in theta_list:
+        print 'processing theta: ',t
+        
+        filelist = []
+        theta = []
+        phi = []
+        
+        for f in glob.glob(path+file_prefix+str(t)+'_*_shield.txt'):
+            filelist.append(f)
+            theta.append(int(f.split('_')[2]))
+            phi.append(int(f.split('_')[3]))
+        
+        #get the 1,1 data which we approximate as correct
+        OneOne = np.genfromtxt(path+file_prefix+'1_1_shield.txt')    
+    
+        BoxDataList = []
+        DataList = []
+        Labels = []
+        
+        #sort the data by theta so it is plotted in the correct order
+        sorted_data =  sorted(zip(phi,theta,filelist))
+        theta_sorted = [x[1] for x in sorted_data]
+        filelist_sorted = [x[2] for x in sorted_data]   
+        phi_sorted = [x[0] for x in sorted_data]
+     
+        
+        for a in zip(filelist_sorted,theta_sorted,phi_sorted):
+            tmpArray = np.genfromtxt(a[0])
+            BoxDataList.append(tmpArray)
+            DataList.append(np.amax(np.fabs(OneOne-tmpArray)))
+            Labels.append('('+str(a[1])+', '+str(a[2])+')')
+            
+            
+        plt.boxplot(BoxDataList)
+        plt.plot(range(1,len(DataList)+1),DataList,'r-',linewidth=2)
+        plt.title('Theta [Azimuth] step size: '+Labels[1].split(',')[0].strip('('))
+        ax = plt.gca()
+        ax.set_xticklabels(Labels,rotation=45)
+        plt.ylim((0,1.2))
+        plt.tight_layout()
+        
+        plt.savefig('Theta_Azimuth_resid_'+Labels[1].split(',')[0].strip('(')+'.png')
+        plt.clf()
+        
+        
+
+resid_comparison_hold_theta()
