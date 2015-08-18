9c2f3fa82913ddabd665b9bff4477387169280e3
Stuart Grieve <s.grieve@ed.ac.uk>
2015-08-18 09:46:37 +0100

    New version that does different stacks

--- stack_images.py
+++ stack_images.py
@@ -8,20 +8,56 @@ Created on Thu Aug 06 20:39:57 2015
 import Image
 import os
 
+def three_stack(path):
 
-path = 'C:\\Users\\Stuart\\Dropbox\\e_r_paper\\figs\\spatial_averaging_comparison\\'
+    files = os.listdir(path)
+    
+    a = Image.open(path+files[0])
+    b = Image.open(path+files[1])
+    c = Image.open(path+files[2])
+        
+    d = Image.new('RGB', (4000, 9000))
+    
+    d.paste(a,(0,0))
+    d.paste(b,(0,3000))
+    d.paste(c,(0,6000))
+    
+    d.save(path+'three_stack.png',quality=100)
+        
+def two_stack(path):
 
-files = os.listdir(path)
+    files = os.listdir(path)
+    
+    a = Image.open(path+files[0])
+    b = Image.open(path+files[1])
+        
+    c = Image.new('RGB', (4000, 6000))
+    
+    c.paste(a,(0,0))
+    c.paste(b,(0,3000))
+    
+    c.save(path+'two_stack.png',quality=100)
+    
+def four_panel(path):
 
-b = Image.open(path+files[0])
-a = Image.open(path+files[1])
-d = Image.open(path+files[2])
+    files = os.listdir(path)
+    
+    a = Image.open(path+files[0])
+    b = Image.open(path+files[1])
+    c = Image.open(path+files[0])
+    d = Image.open(path+files[1])
+        
+    e = Image.new('RGB', (8000, 6000))
+    
+    e.paste(a,(0,0))
+    e.paste(b,(4000,0))
+    e.paste(c,(0,3000))
+    e.paste(d,(4000,3000))
+    
+    e.save(path+'four_panel.png',quality=100)
 
+path = 'C:/Users/Stuart/Dropbox/e_r_paper/figs/avg_method_comparison/'    
 
-c = Image.new('RGB', (4000, 9000))
-
-c.paste(b,(0,0))
-c.paste(a,(0,3000))
-c.paste(d,(0,6000))
-
-c.save(path+'stack.png',quality=100)
+two_stack(path)    
+#three_stack(path)
+#four_panel(path)
