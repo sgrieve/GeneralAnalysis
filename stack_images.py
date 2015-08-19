8b69d544c8e007c756c080a854f40f4ccbcd4cef
Stuart Grieve <s.grieve@ed.ac.uk>
2015-08-19 11:01:31 +0100

    Added subplot labelling

--- stack_images.py
+++ stack_images.py
@@ -6,7 +6,14 @@ Created on Thu Aug 06 20:39:57 2015
 """
 
 import Image
+import ImageFont
+import ImageDraw
 import os
+import string
+
+#set up fonr, supply the path to the font to be used here along with the size of the text
+font = ImageFont.truetype("C:\\Windows\\Fonts\\Arial.ttf",200)
+labels = list(string.ascii_uppercase)[:4]  #plot labels
 
 def three_stack(path):
 
@@ -22,6 +29,16 @@ def three_stack(path):
     d.paste(b,(0,3000))
     d.paste(c,(0,6000))
     
+    
+    draw = ImageDraw.Draw(d)   
+    
+    xPlacements = [0,0,0]
+    yPlacements = [0,3000,6000]
+    
+    for i in range(3):
+        draw.text((600+xPlacements[i], 350+yPlacements[i]),labels[i],(0,0,0),font=font)       
+    
+    
     d.save(path+'three_stack.png',quality=100)
         
 def two_stack(path):
@@ -35,7 +52,16 @@ def two_stack(path):
     
     c.paste(a,(0,0))
     c.paste(b,(0,3000))
+
+    draw = ImageDraw.Draw(c)   
     
+    xPlacements = [0,0]
+    yPlacements = [0,3000]
+    
+    for i in range(2):
+        draw.text((600+xPlacements[i], 350+yPlacements[i]),labels[i],(0,0,0),font=font)   
+           
+
     c.save(path+'two_stack.png',quality=100)
     
 def four_panel(path):
@@ -44,8 +70,8 @@ def four_panel(path):
     
     a = Image.open(path+files[0])
     b = Image.open(path+files[1])
-    c = Image.open(path+files[0])
-    d = Image.open(path+files[1])
+    c = Image.open(path+files[2])
+    d = Image.open(path+files[3])
         
     e = Image.new('RGB', (8000, 6000))
     
@@ -53,11 +79,19 @@ def four_panel(path):
     e.paste(b,(4000,0))
     e.paste(c,(0,3000))
     e.paste(d,(4000,3000))
+
+    draw = ImageDraw.Draw(e)   
+    
+    xPlacements = [0,4000,0,4000]
+    yPlacements = [0,0,3000,3000]
+    
+    for i in range(4):
+        draw.text((600+xPlacements[i], 350+yPlacements[i]),labels[i],(0,0,0),font=font)   
     
     e.save(path+'four_panel.png',quality=100)
 
-path = 'C:/Users/Stuart/Dropbox/e_r_paper/figs/avg_method_comparison/'    
+path = 'C:/Users/Stuart/Dropbox/e_r_paper/figs/nc_data/'    
 
-two_stack(path)    
+#two_stack(path)    
 #three_stack(path)
-#four_panel(path)
+four_panel(path)
